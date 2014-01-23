d0dfad7f1619199ee9abb5bd35da64b95665e784
Aaron Harpole <aharpole@gmail.com>
2014-01-23 14:56:24 -0800

    raise an error in case YouTube doesn't give back a 200 response when we get the channel's uploads feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,7 @@
 class FeedController < ApplicationController
   def for_channel
     resp = HTTParty.get("http://gdata.youtube.com/feeds/api/users/#{params[:channel_name]}/uploads?v=2&alt=json")
+    raise "Bad YouTube response" if resp.code != 200
     @feed_entries = feed_entries(resp.body)
     
     respond_to do |format|
